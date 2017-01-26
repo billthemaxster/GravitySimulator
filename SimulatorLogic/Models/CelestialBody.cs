c465e98341ad3e7e808de498ffedfdbb6846b5d2
billthemaxster <billthemaxster@gmail.com>
2017-01-26 16:59:34 +0000

    Add CelestialBody class

---
+++ SimulatorLogic/Models/CelestialBody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatorLogic.Models
+{
+    public class CelestialBody
+    {
+        public Vector Postition { get; set; }
+        public Vector Velocity { get; set; }
+        public Vector Acceleration { get; set; }
+        public double Mass { get; set; }
+
+    }
+}
